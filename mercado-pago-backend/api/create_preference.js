7219c5daf4bdb956a7b7faf033f8980feeb3b3cc
Flavio silva <gffrangoassado@gmail.com>
2025-06-27 05:53:03 -0300

    Add files via upload

---
+++ mercado-pago-backend/api/create_preference.js
@@ -0,0 +1,28 @@
+const mercadopago = require('mercadopago');
+
+mercadopago.configure({
+  access_token: 'APP_USR-8013618126724232-062520-bd2eca4970ed268d2a29b7b7ea28c145-314621968'
+});
+
+module.exports = async (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Método não permitido' });
+  }
+  try {
+    const { items } = req.body;
+    const preference = {
+      items: items,
+      back_urls: {
+        success: "https://coliseum-shop.netlify.app",
+        failure: "https://coliseum-shop.netlify.app",
+        pending: "https://coliseum-shop.netlify.app"
+      },
+      auto_return: "approved"
+    };
+    const response = await mercadopago.preferences.create(preference);
+    res.status(200).json({ id: response.body.id });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Erro ao criar preferência do Mercado Pago." });
+  }
+};
